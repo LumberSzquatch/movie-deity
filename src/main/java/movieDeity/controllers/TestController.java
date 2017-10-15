5744d5fe648a3b5762d9c5a5bebf2a5e8a3e32ae
ZFenton <zfenton@siue.edu>
2017-10-15 00:25:46 -0500

    added props file to fix error

--- src/main/java/movieDeity/controllers/TestController.java
+++ src/main/java/movieDeity/controllers/TestController.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class TestController {
 
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
+
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
