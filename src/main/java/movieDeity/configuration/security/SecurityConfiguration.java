9deea2ad2478cd6b45148b67b83432b6f0fe8527
ZFenton <zfenton@siue.edu>
2017-11-10 06:49:17 -0600

    Externalized css and js. Js still not functional

--- src/main/java/movieDeity/configuration/security/SecurityConfiguration.java
+++ src/main/java/movieDeity/configuration/security/SecurityConfiguration.java
@@ -9,7 +9,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll(); // WILL NEED TO REVISIT THIS LATER
+        httpSecurity.authorizeRequests().antMatchers("../").permitAll(); // WILL NEED TO REVISIT THIS LATER
     }
 
 }
